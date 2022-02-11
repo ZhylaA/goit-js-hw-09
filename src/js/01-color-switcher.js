eb58e6de157f9301d46736a912f6cc3bfadbc750
ZhylaA <zak2010@i.ua>
2022-02-11 16:27:26 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -15,3 +15,30 @@
 
 // function getRandomHexColor() {
 //   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const date = new Date();
+console.log("Date: ", date);
+
+// Возвращает день месяца от 1 до 31
+console.log("getDate(): ", date.getDate());
+
+// Возвращает день недели от 0 до 6
+console.log("getDay(): ", date.getDay());
+
+// Возвращает месяц от 0 до 11
+console.log("getMonth(): ", date.getMonth());
+
+// Возвращает год из 4 цифр
+console.log("getFullYear(): ", date.getFullYear());
+
+// Возвращает час
+console.log("getHours(): ", date.getHours());
+
+// Возвращает минуты
+console.log("getMinutes(): ", date.getMinutes());
+
+// Возвращает секунды
+console.log("getSeconds(): ", date.getSeconds());
+
+// Возвращает миллисекунды
+console.log("getMilliseconds(): ", date.getMilliseconds());
