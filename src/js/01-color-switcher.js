af3decf9d6319559014160fe094262781c33d24d
ZhylaA <zak2010@i.ua>
2022-02-14 13:53:38 +0200

    hw t1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,36 +1,31 @@
-
-// Задание 1 - переключатель цветов
-//  Посмотри демо видео работы переключателя.
-// В HTML есть кнопки «Start» и «Stop».
-
-// <button type="button" data-start>Start</button>
-// <button type="button" data-stop>Stop</button>
-// Напиши скрипт, который после нажатия кнопки «Start», раз в секунду меняет цвет 
-// фона < body > на случайное значение используя инлайн стиль. 
-// При нажатии на кнопку «Stop», изменение цвета фона должно останавливаться.
-
-// ⚠️ Учти, на кнопку «Start» можно нажать бесконечное количество раз. 
-// Сделай так, чтобы пока изменение темы запушено, кнопка «Start» была не активна(disabled).
-
-// Для генерации случайного цвета используй функцию getRandomHexColor.
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
-// const startBtn = document.querySelector('button[data-start]');
-// console.log(startBtn);
-//     startBtn.style.backgroundColor = getRandomHexColor;
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyBgd = document.querySelector('body');
+let timerId = null;
 
-// startBtn.addEventListener("click", () => {
-//     function getRandomHexColor() {
-//         return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-//     }
-// });
-    
+startBtn.addEventListener("click", startClick);
+// при нажатии на кнопку старт меняет цвет боди 1з в секунду
+function startClick() {
+    timerId = setInterval(() => {
+        bodyBgd.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+// startBtn.setAttribute('disabled', 'true');
+// stopBtn.setAttribute('disabled', 'false');
 
-    
-    
-//     background => (){ background.style.backgroundColor }
-    
-// console.log("Button was clicked");
-// });
+startBtn.disabled = true;
+stopBtn.disabled = false;
+};
+stopBtn.addEventListener("click", stopClick);
+function stopClick() {
+    // останавливает
+    clearInterval(timerId);
+
+    // startBtn.removeAttribute('disabled');
+    // stopBtn.setAttribute('disabled', 'false');
+startBtn.disabled = false;
+stopBtn.disabled = true;
+};
