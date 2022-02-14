abdfdbb4c1ca27b06d752becbc7b10e8ade5d75b
ZhylaA <zak2010@i.ua>
2022-02-14 10:53:39 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,11 +1,12 @@
 
 // Задание 1 - переключатель цветов
-// Выполняй это задание в файлах 01-color-switcher.html и 01-color-switcher.js. Посмотри демо видео работы переключателя.
+//  Посмотри демо видео работы переключателя.
 // В HTML есть кнопки «Start» и «Stop».
 
 // <button type="button" data-start>Start</button>
 // <button type="button" data-stop>Stop</button>
-// Напиши скрипт, который после нажатия кнопки «Start», раз в секунду меняет цвет фона <body> на случайное значение используя инлайн стиль. 
+// Напиши скрипт, который после нажатия кнопки «Start», раз в секунду меняет цвет 
+// фона < body > на случайное значение используя инлайн стиль. 
 // При нажатии на кнопку «Stop», изменение цвета фона должно останавливаться.
 
 // ⚠️ Учти, на кнопку «Start» можно нажать бесконечное количество раз. 
@@ -13,32 +14,23 @@
 
 // Для генерации случайного цвета используй функцию getRandomHexColor.
 
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
-const date = new Date();
-console.log("Date: ", date);
+// const startBtn = document.querySelector('button[data-start]');
+// console.log(startBtn);
+//     startBtn.style.backgroundColor = getRandomHexColor;
 
-// Возвращает день месяца от 1 до 31
-console.log("getDate(): ", date.getDate());
+// startBtn.addEventListener("click", () => {
+//     function getRandomHexColor() {
+//         return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+//     }
+// });
+    
 
-// Возвращает день недели от 0 до 6
-console.log("getDay(): ", date.getDay());
-
-// Возвращает месяц от 0 до 11
-console.log("getMonth(): ", date.getMonth());
-
-// Возвращает год из 4 цифр
-console.log("getFullYear(): ", date.getFullYear());
-
-// Возвращает час
-console.log("getHours(): ", date.getHours());
-
-// Возвращает минуты
-console.log("getMinutes(): ", date.getMinutes());
-
-// Возвращает секунды
-console.log("getSeconds(): ", date.getSeconds());
-
-// Возвращает миллисекунды
-console.log("getMilliseconds(): ", date.getMilliseconds());
+    
+    
+//     background => (){ background.style.backgroundColor }
+    
+// console.log("Button was clicked");
+// });
