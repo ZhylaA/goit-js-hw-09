a528a7c94016ecc7f94dbb7fb86384c343db52b5
ZhylaA <zak2010@i.ua>
2022-02-18 09:55:02 +0200

    3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,13 @@
+import Notiflix from 'notiflix';
+
+// В HTML есть разметка формы, в поля которой пользователь будет вводить первую задержку в миллисекундах,
+//   шаг увеличения задержки для каждого промиса после первого и количество промисов которое необходимо создать.
+const form = document.querySelector('.form');
+const button = document.querySelector('button');
+let delay = document.querySelector('input[name="delay"]');
+let step = document.querySelector('input[name="step"]');
+let amount = document.querySelector('input[name="amount"]');
+form.addEventListener('submit', createPromise);
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -5,44 +15,13 @@ function createPromise(position, delay) {
   } else {
     // Reject
   }
-}
-// Задание 3 - генератор промисов
-// Выполняй это задание в файлах 03-promises.html и 03-promises.js. Посмотри демо видео работы генератора промисов.
-// В HTML есть разметка формы, в поля которой пользователь будет вводить первую задержку в миллисекундах,
-//   шаг увеличения задержки для каждого промиса после первого и количество промисов которое необходимо создать.
-
-// <form class="form">
-//   <label>
-//     First delay (ms)
-//     <input type="number" name="delay" required />
-//   </label>
-//   <label>
-//     Delay step (ms)
-//     <input type="number" name="step" required />
-//   </label>
-//   <label>
-//     Amount
-//     <input type="number" name="amount" required />
-//   </label>
-//   <button type="submit">Create promises</button>
-// </form>
 //   Напиши скрипт, который при сабмите формы вызывает функцию createPromise(position, delay) столько раз,
 //   сколько ввели в поле amount.При каждом вызове передай ей номер создаваемого промиса(position) 
 //     и задержку учитывая введенную пользователем первую задержку(delay) и шаг(step).
-
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
 // Дополни код функции createPromise так, чтобы она возвращала один промис,
 //   который выполянется или отклоняется через delay времени.Значением промиса должен быть объект,
 //   в котором будут свойства position и delay со значениями одноименных параметров.
 //     Используй начальный код функции для выбора того, что нужно сделать с промисом - выполнить или отклонить.
-
 // createPromise(2, 1500)
 //   .then(({ position, delay }) => {
 //     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
@@ -52,5 +31,4 @@ function createPromise(position, delay) {
 //   });
 // Библиотека уведомлений
 // ⚠️ Следующий функционал не обязателен при сдаче задания, но будет хорошей дополнительной практикой.
-
-// Для отображения уведомлений пользователю вместо console.log() используй библиотеку notiflix.
+// Для отображения уведомлений пользователю вместо console.log() используй библиотеку notiflix
